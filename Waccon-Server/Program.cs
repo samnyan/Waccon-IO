5a913c4f187634ff7cca872ff8931b22523b8d06
samnyan <4137880+samnyan@users.noreply.github.com>
2026-09-04 16:03:56 +0800

    chore(waccon): simplify server network startup output

--- Waccon-Server/Program.cs
+++ Waccon-Server/Program.cs
@@ -11,32 +11,25 @@ Console.CancelKeyPress += (_, e) =>
     cts.Cancel();
 };
 
-Console.WriteLine($"Waccon Server listening on {options.ListenAddress}:{options.TcpPort}");
-Console.WriteLine("Network addresses:");
+Console.WriteLine($"Waccon Server tcp listening on {options.ListenAddress}:{options.TcpPort}");
+Console.WriteLine($"Waccon Server websocket listening on {options.ListenAddress}:{options.WebPort}");
+Console.WriteLine("Web controller:");
 var addresses = NetworkInterface.GetAllNetworkInterfaces()
     .Where(networkInterface => networkInterface.OperationalStatus == OperationalStatus.Up)
     .SelectMany(networkInterface => networkInterface.GetIPProperties().UnicastAddresses
         .Where(unicast => unicast.Address.AddressFamily == AddressFamily.InterNetwork)
-        .Select(unicast => unicast.Address)))
+        .Select(unicast => unicast.Address))
+    .Append(IPAddress.Loopback)
     .Distinct()
     .OrderBy(address => address.Equals(IPAddress.Loopback))
     .ThenBy(address => address.ToString(), StringComparer.Ordinal)
     .ToArray();
 
-if (addresses.Length == 0)
+foreach (var address in addresses)
 {
-    Console.WriteLine("  (no IPv4 network addresses found)");
-}
-else
-{
-    foreach (var address in addresses)
-    {
-        Console.WriteLine($"  {address}");
-        Console.WriteLine($"    Web: http://{address}:{options.WebPort}/web/");
-    }
+    Console.WriteLine($"    http://{address}:{options.WebPort}/web/");
 }
 
-Console.WriteLine($"Web controller: http://{options.ListenAddress}:{options.WebPort}/web/");
 Console.WriteLine($"Shared memory: {options.SharedMemoryName}");
 await using var server = new WacconServer(options);
 await server.RunAsync(cts.Token).ConfigureAwait(false);
